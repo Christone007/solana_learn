a5ba7a04ce2c64bee47ab66f4a851047e90455b4
Christone007 <exellentemy@gmail.com>
2025-09-17 14:46:21 +0100

    Check live balances on mainnet-beta

--- 3-check_balance.ts
+++ 3-check_balance.ts
@@ -1,7 +1,13 @@
 import { Connection, PublicKey, LAMPORTS_PER_SOL, clusterApiUrl } from "@solana/web3.js";
 
-const publicKey = new PublicKey("33cUmuYgn7PB3GJjorT6uFNp1KoYx8852uhhQgy9ynY7");
-const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+const suppliedPublicKey = process.argv[2];
+
+if (!suppliedPublicKey) {
+    throw new Error("Provide a public key to check balance")
+}
+
+const publicKey = new PublicKey(suppliedPublicKey);
+const connection = new Connection(clusterApiUrl("mainnet-beta"), "confirmed");
 const balanceInLamports = await connection.getBalance(publicKey);
 const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
 console.log(`💰 The balance on wallet at addr ${publicKey} is ${balanceInSOL} SOL (${balanceInLamports} Lamports!)`);
